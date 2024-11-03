d57985fe12436d352790b6c192656f4569714439
Ichim-Andronache George <george_ichim101@yahoo.com>
2024-11-03 20:41:12 +0200

    backend adaugat

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express=require('express')
+const app=express()
+const path=require("path")
+
+app.use(express.static(path.join(__dirname,'../')))
+app.set('views',path.join(__dirname,'../'))
+app.set('view engine','ejs')
+
+app.get("/",(req,res)=>
+{
+    res.render('index')
+})
+app.listen(3000)
